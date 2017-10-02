bc07d33a2efdb9bb7a3615cb438391fcc2159a3d
Marc <marc.schlichte@gmail.com>
2017-10-02 23:55:41 +0200

    Making map function async.

--- Sources/Stream.swift
+++ Sources/Stream.swift
@@ -3,14 +3,19 @@ import Foundation
 public extension Observable {
     
     /// Creates another `Observable` by mapping the value of the `Observer` to the generic type `T`.
-    func map<T>(_ f: @escaping (Value) -> T) -> Signal<T> {
+    func map<T>(_ f: @escaping (Value) -> T, queue: DispatchQueue, cont: @escaping Continuation<Signal<T>>) {
         
         let processor = Signal<T>(name: "MapProcessor", disposeOnLastDisconnect: true)
         
         connect(ClosureSlot(actor: processor) { value in
             processor.emit(f(value))
-        }, queue: processor.actorQueue, cont: processor.disposeBag.add)
-        
-        return processor
+            
+        }, queue: processor.actorQueue) { connection in
+            processor.disposeBag.add(connection)
+            
+            queue.async {
+                cont(processor)
+            }
+        }
     }
 }
--- Tests/actressTests/StreamTests.swift
+++ Tests/actressTests/StreamTests.swift
@@ -12,26 +12,55 @@ class StreamTests: XCTestCase {
         let s = Signal<Int>()
         
         var detectCount = 0
-
-        s.map { $0 * 2 }.connect(ClosureSlot(queue: .main) { value in
-            XCTAssertEqual(value, 42)
-            
-            XCTAssertEqual(detectCount, 0)
-            detectCount += 1
-            
-            db1.dispose()
-            
-            s.emit(0)
-            
-        }, queue: .main, cont: db1.add)
         
+        s.map({ $0 * 2 }, queue: .main) { processor in
+            processor.connect(ClosureSlot(queue: .main) { value in
+                XCTAssertEqual(value, 42)
+                
+                XCTAssertEqual(detectCount, 0)
+                detectCount += 1
+                
+                db1.dispose()
+                
+                s.emit(0)
+                
+            }, queue: .main) { connection in
+                db1.add(connection)
+                
+                s.emit(21)
+            }
+        }
+
         s.connect(ClosureSlot(queue: .main) { value in
             if value == 0 {
                 expectation.fulfill()
             }
         }, queue: .main, cont: db2.add)
+
+        waitForExpectations(timeout: 7)
+    }
+    
+    func testMultiMap() {
+        let expectation = self.expectation(description: #function)
         
-        s.emit(21)
+        let db = DisposeBag()
+        
+        let s = Signal<Int>()
+
+        s.map({ (val: Int) -> Int in val * 2 }, queue: .main) { proc in
+            proc.map({ "\($0)" }, queue: .main) { proc in
+                proc.connect(ClosureSlot(queue: .main) { value in
+                    XCTAssertEqual(value, "42")
+                    
+                    expectation.fulfill()
+                    
+                }, queue: .main) { conn in
+                    db.add(conn)
+                    
+                    s.emit(21)
+                }
+            }
+        }
         
         waitForExpectations(timeout: 7)
     }
